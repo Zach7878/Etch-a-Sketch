c64f5aa0a9fcc17f097bcd36724711fdaad9f5af
Zach7878 <zacharyzhang7@gmail.com>
2023-10-02 17:57:13 -0400

    add change size button

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const container=document.querySelector('#container')
-for(let i=0;i<256;i++){
-    const gridBox=document.createElement('div')
-    gridBox.textContent="bruh";
-    gridBox.classList.add('gridBox')
-    gridBox.style.width='100px';
-    gridBox.style.backgroundColor='blue';
-    gridBox.style.border='2px solid black';
-    container.appendChild(gridBox);
+const button=document.querySelector('button')
+let numberOfBoxes;
+let flexPercentage;
+button.addEventListener('click',()=>{
+    numberOfBoxes=prompt("How many squares do you want on each side?")
+    if (numberOfBoxes>100){
+        alert("Cannot be over 100 squares or it lags")
+    }
+    else if (numberOfBoxes===null){
+        alert("No input")
+    }
+    else if(numberOfBoxes<1){
+        alert("Cannot have negative or no squares")
+    }
+    else{
+        const gridBoxes=document.querySelectorAll('.gridBox');
+        gridBoxes.forEach((gridBox)=>{
+            container.removeChild(gridBox);
+        })
+        flexPercentage=100/numberOfBoxes;
+        makeBoxes();
+    }
+})
+function makeBoxes(){
+    for(let i=0;i<numberOfBoxes*numberOfBoxes;i++){
+        const gridBox=document.createElement('div')
+        gridBox.classList.add('gridBox')
+        gridBox.style.width='100px';
+        gridBox.style.backgroundColor='blue';
+        gridBox.style.border='2px solid black';
+        gridBox.style.flex=`0 0 calc(${flexPercentage}% - 4px)`;
+        container.appendChild(gridBox);
+
+    }
+    const gridBoxes=document.querySelectorAll('.gridBox');
+    gridBoxes.forEach((gridBox)=>{
+        gridBox.addEventListener('mouseenter',()=>{
+            gridBox.style.backgroundColor='black';
+        });
+});
 
 }
-const gridBoxes=document.querySelectorAll('.gridBox');
-gridBoxes.forEach((gridBox)=>{
-    gridBox.addEventListener('mouseenter',()=>{
-        gridBox.style.backgroundColor='black';
-    });
-});
+
